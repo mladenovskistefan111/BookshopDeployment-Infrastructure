da87762aaa0467f3742ec7099abc5eee1eca3018
Stefan Mladenovski <165105364+mladenovskistefan111@users.noreply.github.com>
2024-09-19 01:18:06 +0200

    Update main.tf

--- database/main.tf
+++ database/main.tf
@@ -1,5 +1,7 @@
 # --- database/main.tf ---
 
+# Get secret data from secrets manager
+
 data "aws_secretsmanager_secret" "db_secret" {
   name = "DbSecrets"
 }
@@ -8,9 +10,21 @@ data "aws_secretsmanager_secret_version" "db_secret_version" {
   secret_id = data.aws_secretsmanager_secret.db_secret.id
 }
 
+# Get EKS Security Group
+
+data "aws_eks_cluster" "eks" {
+  name = var.cluster_name
+}
+
+data "aws_security_group" "eks_default_sg" {
+  id = data.aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
+}
+
+# Create Security Group for RDS and add rules
+
 resource "aws_security_group" "db_security_group" {
   name        = "db_security_group"
-  description = "Security group for DB to communicate with EKS"
+  description = "Security group for DB"
   vpc_id      = var.vpc_id
 }
 
@@ -23,15 +37,16 @@ resource "aws_security_group_rule" "allow_bastion_to_rds" {
   source_security_group_id = var.bastion_security_group_id
 }
 
-
 resource "aws_security_group_rule" "allow_app_ingress" {
   type                     = "ingress"
   from_port                = 5432
   to_port                  = 5432
   protocol                 = "tcp"
   security_group_id        = aws_security_group.db_security_group.id
-  source_security_group_id = var.app_security_group_id
+  source_security_group_id = data.aws_security_group.eks_default_sg.id
 }
+
+# Create RDS Database
 
 resource "aws_db_instance" "db" {
   count                  = var.db_instance_count
